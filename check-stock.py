54e0290408914a3c3d1e4320e86f92ddd6505eef
Andy Liu <andyliu60@outlook.com>
2019-01-02 21:21:44 +0800

    modified codes

--- check-stock.py
+++ check-stock.py
@@ -10,7 +10,7 @@ token = '27ecfe3c4981fc3d653e9cd70cfedf1e87787d01253f6282706a4e2e'
 pro = ts.pro_api(token)
 ifttt_event = 'stock_event'
 
-stocks = ({'name':'永辉超市', 'symbol':'601933.SH'},{'name':'大华股份', 'symbol':'002236.SZ'})
+stocks = ({'name':'永辉超市', 'symbol':'601933.SH', 'cost':9.499},{'name':'大华股份', 'symbol':'002236.SZ', 'cost':25.073})
 today = datetime.now().day
 
 for stock in stocks:
@@ -18,8 +18,9 @@ for stock in stocks:
     stock_trade_day = datetime.strptime(stock_df.trade_date[0], '%Y%m%d').day
 
     if stock_trade_day != today:
-        Notification(value1 = stock['name'], value2 = '数据没有更新', event = stock['event']).sent()
+        Notification(value1 = stock['name'], value2 = '数据没有更新', event = ifttt_event).sent()
     else:
         stock_price = stock_df.close[0]
+        cost_pchange = round((stock_price - stock['cost']) / stock['cost'] * 100, 2)
         stock_pchange = '%+.2f%%' % round(stock_df.pct_chg[0],4)
-        Notification(value1 = stock['name'], value2 = '收盘价：%.2f' % stock_price + ' | ' + '涨跌幅：' + stock_pchange, event = ifttt_event).sent()
+        Notification(value1 = stock['name'], value2 = '收盘价：%.2f' % stock_price + ' | ' + '涨跌幅：' + stock_pchange + ' |  ' + '盈亏：%+.2f%%' % cost_pchange, event = ifttt_event).sent()
