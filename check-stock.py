c32e7ff347376131acd7a8b4e09e46dee27f365d
andyliu60 <andyliu60@outlook.com>
2019-01-02 15:22:42 +0800

    changed the codes

--- check-stock.py
+++ check-stock.py
@@ -6,36 +6,19 @@ import tushare as ts
 from ifttt import Notification
 from datetime import datetime
 
-token = ''
-
+token = '27ecfe3c4981fc3d653e9cd70cfedf1e87787d01253f6282706a4e2e'
 pro = ts.pro_api(token)
 
-yhcs = '601933.SH'
-dhgf = '002236.SZ'
-yhcs_event = 'yhcs_close'
-dhgf_event = 'dhgf_close'
-
+stocks = ({'name':'永辉超市', 'symbol':'601933.SH', 'event':'yhcs_close'},{'name':'大华股份', 'symbol':'002236.SZ', 'event':'dhgf_close'})
 today = datetime.now().day
 
-yhcs_df = pro.daily(ts_code=yhcs)
-dhgf_df = pro.daily(ts_code=dhgf)
-yhcs_trade_day = datetime.strptime(yhcs_df.trade_date[0], '%Y%m%d').day
-dhgf_trade_day = datetime.strptime(dhgf_df.trade_date[0], '%Y%m%d').day
+for stock in stocks:
+    stock_df = pro.daily(ts_code=stock['symbol'])
+    stock_trade_day = datetime.strptime(stock_df.trade_date[0], '%Y%m%d').day
 
-if yhcs_trade_day != today:
-    Notification(value1 = '数据没有更新', event = yhcs_event).sent()
-
-else:
-    
-    yhcs_price = yhcs_df.close[0]
-    yhcs_pchange = '%+.2f%%' % round(yhcs_df.pct_chg[0],4)
-    Notification(value1 = '收盘价：%.2f' % yhcs_price, value2 = '涨跌幅：' + yhcs_pchange, event = yhcs_event).sent()
-
-if dhgf_trade_day != today:
-    Notification(value1 = '数据没有更新', event = dhgf_event).sent()
-
-else:
-    
-    dhgf_price = dhgf_df.close[0]
-    dhgf_pchange = '%+.2f%%' % round(dhgf_df.pct_chg[0],4)
-    Notification(value1 = '收盘价：%.2f' % dhgf_price, value2 = '涨跌幅：' + dhgf_pchange, event = dhgf_event).sent()
+    if stock_trade_day != today:
+        Notification(value1 = '数据没有更新', event = stock['event']).sent()
+    else:
+        stock_price = stock_df.close[0]
+        stock_pchange = '%+.2f%%' % round(stock_df.pct_chg[0],4)
+        Notification(value1 = '收盘价：%.2f' % stock_price, value2 = '涨跌幅：' + stock_pchange, event = stock['event']).sent()
